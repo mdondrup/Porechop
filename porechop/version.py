e2ed0aaf834549df0b2907c1925ff4cbb4a4348a
Michael Dondrup <mdondrup@users.noreply.github.com>
2024-02-06 10:03:17 +0100

    Update version.py

--- porechop/version.py
+++ porechop/version.py
@@ -2,4 +2,4 @@
 The version is stored here in a separate file so it can exist in only one place.
 http://stackoverflow.com/questions/458550
 """
-__version__ = '0.2.4_md-adapters-2-2024'
+__version__ = '0.2.4-1'
